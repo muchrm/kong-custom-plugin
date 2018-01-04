a01ec6d35ebb27e3b3c078a403cd09575a6cdbd0
Pongpanot Chuaysakun <muchrm.me@gmail.com>
2018-01-04 17:12:59 +0700

    update credential login

--- plugins/mis-login/access.lua
+++ plugins/mis-login/access.lua
@@ -1,15 +1,51 @@
 local cjson = require "cjson"
 local jwt = require "resty.jwt"
+local singletons = require "kong.singletons"
+local responses = require "kong.tools.responses"
+
+local function retrieve_credentials()
+    ngx.req.read_body()
+    local text = ngx.var.request_body
+    local value = cjson.decode(text)
+    return value["username"],value["password"];
+end
+
+local function fetch_acls(username)
+    print(username)
+    local results, err = singletons.dao.acls:find_all {username = username}
+    if err then
+      return nil, err
+    end
+    return results
+end
+  
+local function load_credential(username)
+    print(username)
+    local rows, err = singletons.dao.jwt_secrets:find_all {username = username, algorithm = "HS256"}
+    if err then
+      return nil, err
+    end
+    return rows[1]
+end
+
 local _M = {}
 
 function _M.execute(conf)
+    local username, password = retrieve_credentials()
+    local acls, err = fetch_acls(username)
+
+    if err then
+        return responses.send_HTTP_INTERNAL_SERVER_ERROR(err)
+    end
+    
+    local credential, err = load_credential(consumer_id)
 
     local jwt_token = jwt:sign(
         "lua-resty-jwt",
         {
         header = {
             typ = "JWT",
-            alg = "HS256" -- load_credential only loads HS256 for now
+            alg = "HS256"
         },
         payload = {
             sub = "clerk",
